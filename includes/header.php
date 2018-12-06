c860c7fce1ecb56b43f23092edf165874ed3a29b
Jcalderin91 <jesuscalderin91@gmail.com>
2018-12-05 20:29:37 -0400

    añadir libreria que faltaba

--- includes/header.php
+++ includes/header.php
@@ -1,22 +1,23 @@
-<?php require_once 'conexion.php'; ?>
+<?php //require_once 'conexion.php'; ?>
 
 <!doctype html>
 <html lang="es">
   <head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		
+		<!-- ARCHIVOS CSS BOOTSTRAP 4 -->
+		<link type="text/css" rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<link type="text/css" rel="stylesheet" href="assets/css/styles.min.css">
+		<link type="text/css" rel="stylesheet" href="assets/css/mdb.min.css">
 
-	
-	
-	<!-- ARCHIVOS CSS BOOTSTRAP 4 -->
-	<link type="text/css" rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<!-- ARCHIVOS CSS PERSONALIZADOS-->
+		<link type="text/css" rel="stylesheet" href="assets/css/style.css">
+	 
 
-	<!-- ARCHIVOS CSS PERSONALIZADOS-->
-	<link type="text/css" rel="stylesheet" href="assets/css/style.css">
- 
-
-	<title>Blog de Videojuegos</title>
+		<title>Blog de Videojuegos</title>
   </head>
   <body>
 	
